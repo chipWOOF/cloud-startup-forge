130ffcaed33733034652d7ecf0499eeaeb6b5e10
Minh Bao Bui <minhbui5002@gmail.com>
2025-08-04 12:35:22 +1000

    again

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="signin" element={<SignIn />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
